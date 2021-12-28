32425c9b06a5b11458707b27e000a8fa1b1063bd
BKuipers <73161295+BKuipers@users.noreply.github.com>
2021-12-28 15:17:06 +0100

    progam.py opgeruimd

--- program.py
+++ program.py
@@ -9,18 +9,20 @@ Highpass = 2200  # Remove lower frequencies.
 Lowpass = 15000  # Remove higher frequencies.
 
 
-def get_audio_data(filename):
-    audio_read = wave.open(filename, 'r')
+def get_audio_data(audio):
     audio_data = {}  # dictionary voor audio parameters
-    parameters = list(audio_read.getparams())  # Get the parameters from the input.
+    parameters = list(audio.getparams())  # Get the parameters from the input.
     # (nchannels, sampwidth, framerate, nframes, comptype, compname)
+
     audio_data["nchannels"] = parameters[0]
     audio_data["sampwidth"] = parameters[1]
-    audio_data["rate"] = parameters[2]
-    audio_data["nframes"] = parameters[3] = 0  # aantal samples wordt bepaald door "writeframes" functie
+    audio_data["framerate"] = parameters[2]
+    audio_data["nframes"] = parameters[3]  # aantal samples wordt bepaald door "writeframes" functie
+    audio_data["comptype"] = parameters[4]
+    audio_data["compname"] = parameters[5]
     # This file is stereo, 2 bytes/sample, 44.1 kHz.
     # paramters[3] = 0  # The number of samples will be set by writeframes.
-    return audio_read, parameters, audio_data
+    return audio_data
 
 
 def get_audiofiles(folder):
@@ -44,7 +46,7 @@ def plot_audio(title, audio_data, plot_data):
     plt.show()
 
 
-def make_new_file(file_name, directory, parameters):
+def make_new_file(file_name, directory, audio_data):
     # Open the output file
     i = 0
     # directory = __file__
@@ -53,25 +55,43 @@ def make_new_file(file_name, directory, parameters):
         i += 1
     new_file_dir = (directory + "\\" + "Python_made_audiofiles" + "\\" + file_name)
     audio_file = wave.open(new_file_dir, "w")
-    audio_file.setparams(tuple(parameters))  # Use the same parameters as the input file.
+
+    audio_file.setnchannels(audio_data["nchannels"])
+    audio_file.setsampwidth(audio_data["sampwidth"])
+    audio_file.setframerate(audio_data["framerate"])
+    audio_file.setnframes(audio_data["nframes"])
+    audio_file.setcomptype(audio_data["comptype"], audio_data["compname"])
+
     return audio_file
 
 
-def proces_audio(audio_read, audio_write):
-    sz = audio_read.getframerate()  # Read and process 1 second at a time.
-    c = int(audio_read.getnframes() / sz)  # whole file
-    for num in range(c):
-        print('Processing {}/{} s'.format(num + 1, c))
-        da = np.fromstring(audio_read.readframes(sz), dtype=np.int16)
-        left, right = da[0::2], da[1::2]  # left and right channel
-        lf, rf = np.fft.rfft(left), np.fft.rfft(right)  # FFT zodat filters toegepast kunnen worden
-        lf[:Highpass], rf[:Highpass] = 0, 0  # High Pass >2.2kHz
-        lf[55:66], rf[55:66] = 0, 0  # Notch filter 55-66Hz
-        lf[Lowpass:], rf[Lowpass:] = 0, 0  # Low Pass <15kHz
-        nl, nr = np.fft.irfft(lf), np.fft.irfft(rf)  # inverse FFT zodat er weer audio gemaakt van wordt
-        # ns = np.column_stack((nl,nr)).ravel().astype(np.int16)
-        ns = np.column_stack((left, right)).ravel().astype(np.int16)
-        audio_write.writeframes(ns.tostring())
+def HighPass(leftFourier, rightFourier, fk):
+    leftFourier[:fk], rightFourier[:fk] = 0, 0
+    return leftFourier, rightFourier
+
+
+def LowPass(leftFourier, rightFourier, fk):
+    leftFourier[fk:], rightFourier[fk:] = 0, 0
+    return leftFourier, rightFourier
+
+
+def BandSper(leftFourier, rightFourier, fkLow, fkHigh):
+    leftFourier[fkLow:fkHigh], rightFourier[fkLow:fkHigh] = 0, 0
+    return leftFourier, rightFourier
+
+
+def inv_fourier_transform():
+    nl, nr = np.fft.irfft(leftFourier), np.fft.irfft(
+        rightFourier)  # inverse FFT zodat er weer audio gemaakt van wordt
+    ns = np.column_stack((nl, nr)).ravel().astype(np.int16)
+
+
+def frourier_transform():
+    pass
+
+
+def save_file():
+    pass
 
 
 def main():
@@ -84,24 +104,43 @@ def main():
     print("--------------------------------------------")
     chosen_file = int(input("Kies een audiobestand: ")) - 1  # kies audio bestand aan de hadn van nummer
     title = audiofiles[chosen_file]
-    file = directory + "\\" + title
+    audio_file = directory + "\\" + title
 
-    audio_read, parameters, audio_data = get_audio_data(file)
+    input_file = wave.open(audio_file, 'r')
 
-    '''
-    plot_data = []
-    if audio_data["nchannels"] == 2:
-        plot_data.append([audio_data["amplitude"][i] for i in range(len(audio_data["amplitude"])) if i % 2 == 1])
-        plot_data.append([audio_data["amplitude"][i] for i in range(len(audio_data["amplitude"])) if i % 2 == 0])
-    else:
-        plot_data.append(audio_data["amplitude"])
-    '''
+    audio_data = get_audio_data(input_file)
 
-    audio_write = make_new_file(title, directory, parameters)
-    proces_audio(audio_read, audio_write)
+    # plot_data = []
+    # if audio_data["nchannels"] == 2:
+    #     plot_data.append([audio_data["amplitude"][i] for i in range(len(audio_data["amplitude"])) if i % 2 == 1])
+    #     plot_data.append([audio_data["amplitude"][i] for i in range(len(audio_data["amplitude"])) if i % 2 == 0])
+    # else:
+    #     plot_data.append(audio_data["amplitude"])
 
-    audio_read.close()
-    audio_write.close()
+    output_file = make_new_file(title, directory, audio_data)
+
+    duration = int(audio_data["nframes"] / audio_data["framerate"])  # whole file
+
+    for num in range(duration):
+        print('Processing {}/{} s'.format(num + 1, duration))
+        data = np.fromstring(input_file.readframes(audio_data["framerate"]), dtype=np.int16)
+        left, right = data[0::2], data[1::2]  # left and right channel
+        leftFourier, rightFourier = np.fft.rfft(left), np.fft.rfft(right)  # FFT zodat filters toegepast kunnen worden
+        fk = 2200
+        leftFourier, rightFourier = HighPass(leftFourier, rightFourier, fk)
+        fk = 15000
+        leftFourier, rightFourier = LowPass(leftFourier, rightFourier, fk)
+        fkLow = 55
+        fkHigh = 66
+        leftFourier, rightFourier = BandSper(leftFourier, rightFourier, fkLow, fkHigh)
+        nl, nr = np.fft.irfft(leftFourier), np.fft.irfft(
+            rightFourier)  # inverse FFT zodat er weer audio gemaakt van wordt
+        ns = np.column_stack((nl, nr)).ravel().astype(np.int16)
+        # ns = np.column_stack((left, right)).ravel().astype(np.int16)
+        output_file.writeframes(ns.tostring())
+
+    input_file.close()
+    output_file.close()
 
 
 if __name__ == '__main__':
